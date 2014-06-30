af2bc29d72a2f6e7b903843e987f039b61106e97
Jorge Avila <jorge.e.avila@gmail.com>
2014-06-30 17:50:55 -0500

    showing features and news

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ add_action('init', 'sw_register_menus');
 function sw_register_styles(){
 	$path = get_template_directory_uri();
 	wp_register_style("normalize", $path.'/css/normalize.css', array(), "", "screen");
-	wp_register_style("theme", $path.'/css/theme.css', array(), "", "screen");
+	wp_register_style("theme", $path.'/css/style.css', array(), "", "screen");
 }
 add_action('wp_enqueue_scripts','sw_register_styles');
 
@@ -24,4 +24,110 @@ function sw_enqueue_styles(){
 	wp_enqueue_style('theme');
 }
 add_action('wp_enqueue_scripts','sw_enqueue_styles');
+
+/**
+ * Returns actual page id
+ */
+function sw_get_main_id() {
+	return 'some-id';
+}
+
+/**
+ * Set custom post types
+ */
+function sw_create_post_types() {
+	register_post_type('feature', array(
+		'labels' => array(
+			'name' =>__('Features'),
+			'singular_name' => __('Feature')
+			),
+		'public' => true,
+		'has_archive' => true,
+		'supports' => array(
+			'title',
+			'editor',
+			'excerpt',
+			'thumbnail'
+			)
+		));
+}
+add_action('init', 'sw_create_post_types');
+
+/**
+ * View features
+ */
+function sw_list_features($num=3, $do_shortcode=1, 	$strip_shortcodes = 0) {
+	$options = array(
+		'numberposts' => $num,
+		'order_by' => 'menu_order, post_title',
+		'order' => 'DESC',
+		'post_type' => 'feature',
+		'post_status' => 'publish',
+		'supress_filters' => true
+		);
+	$posts = get_posts($options);
+	$html = '';
+
+	foreach ($posts as $post) {
+		$image = get_the_post_thumbnail($post->ID);
+		if (empty($image)) {
+			$img = "<img src='". plugins_url("/img/default.png", __FILE__)."'/>";
+		}
+		if (has_post_thumbnail($post->id)) {
+
+		}
+		$content = $post->post_content;
+		if ($do_shortcode == 1) {
+			$content = do_shortcode($content);
+		}
+		$html .= '
+			<div class="column">
+					<figure>
+					'.$image.'
+					</figure>
+					<h3>'.$post->post_title.'</h3>
+					<div>'.$content.'</div>
+			</div>
+		';
+	}
+	return $html;
+
+}
+
+function sw_list_news($num=3, $do_shortcode=1, 	$strip_shortcodes = 0) {
+	$options = array(
+		'numberposts' => $num,
+		'order_by' => 'menu_order, post_date',
+		'order' => 'DESC',
+		'post_type' => 'post',
+		'post_status' => 'publish',
+		'supress_filters' => true
+		);
+	$posts = get_posts($options);
+	$html = '';
+
+	foreach ($posts as $post) {
+		$image = get_the_post_thumbnail($post->ID);
+		if (empty($image)) {
+			$img = "<img src='". plugins_url("/img/default.png", __FILE__)."'/>";
+		}
+		if (has_post_thumbnail($post->id)) {
+
+		}
+		$content = $post->post_content;
+		if ($do_shortcode == 1) {
+			$content = do_shortcode($content);
+		}
+		$html .= '
+			<div class="post">
+					<h3>'.$post->post_title.'</h3>
+					<div>'.$content.'</div>
+			</div>
+		';
+	}
+	return $html;
+
+}
+
+
 ?>
--- home.php
+++ home.php
@@ -1,7 +1,56 @@
 <?php
 	get_header();
 ?>
-home page
+	<section class="logo">
+		<img src="imgs/sumwars.png" alt="">
+	</section>
+	<div class="image-background">
+		<img src="imgs/priest.jpg" alt="">
+	</div>
+	<article>
+		<section class="download">
+			<div class="wrap">
+				<iframe class="video" width="640" height="360" src="//www.youtube.com/embed/fPXatVmAS7E?rel=0" frameborder="0" allowfullscreen></iframe>
+				<div class="info">
+					<h1>Defeat the infinite dialogs</h1>
+					<p>Get immerse on the intense action and press the skip button. Be a warrior or a lazy wizard. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam diam velit, dapibus in varius et, mattis a massa. Sed ornare nibh quis elit lobortis lobortis varius id arcu. Nunc a dapibus lacus. Etiam vehicula malesuada dolor, non varius mi imperdiet lacinia. Pellentesque ut aliquet massa. In hac habitasse platea dictumst. Aenean eget tempor turpis.</p>
+					<p>
+						<a href="#" class="download-button">Download</a>
+					</p>
+				</div>
+			</div>
+		</section>
+		<section class="features">
+			<div class="wrap">
+				<h2>Features</h2>
+				<?php
+					if (function_exists('sw_list_features')) {
+						echo sw_list_features();
+					}
+				?>
+			</div>
+		</section>
+		<section class="news-activity">
+			<div class="wrap">
+				<div class="news">
+					<h2>News</h2>
+					<?php
+					if (function_exists('sw_list_news')) {
+						echo sw_list_news();
+					}
+					?>
+				</div>
+				<div class="activity">
+					<h2>Some activity</h2>
+					<p>This section could have some interesting stuff on development or may not</p>
+				</div>
+			</div>
+		</section>
+		<section class="contribute">
+			<h2>Contribute</h2>
+		</section>
+	</article>
+
 <?php
 	get_footer();
 ?>
